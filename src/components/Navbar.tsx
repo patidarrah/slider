5b68d208ff2c67105e6709523627d4332ebf2d63
patidarrah <112461092+patidarrah@users.noreply.github.com>
2025-05-13 17:59:54 +0530

    gdfgd

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,30 @@
-
-import "../App.css"; // custom CSS file
+import React, { useState } from "react";
+import "./Navbar.css";
 
 function Navbar() {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar__logo">LOGO</div>
 
-      <ul className="navbar__menu">
+      <div className={`navbar__menu ${isMobileMenuOpen ? "open" : ""}`}>
         <li>Qualifications ▾</li>
         <li>Organizations ▾</li>
         <li>Research & Analysis ▾</li>
         <li>Lorem ipsum ▾</li>
         <li>Lorem ipsum ▾</li>
-      </ul>
+      </div>
 
       <div className="navbar__actions">
         <span className="navbar__search">&#128269;</span>
         <button className="navbar__enrolment">Enrolment</button>
+        <div
+          className="navbar__toggle"
+          onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
+        >
+          ☰
+        </div>
       </div>
     </nav>
   );
